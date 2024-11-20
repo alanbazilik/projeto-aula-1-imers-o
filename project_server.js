a6642d224b50c1d1fa300634804812a19ee9d7c7
alanbazilik <alangoncalvesbazilio@gmail.com>
2024-11-19 21:59:52 -0300

    Aula 2 concluída  ✅

--- project_server.js
+++ project_server.js
@@ -1,23 +1,43 @@
-import express, { json } from "express";
-import { readFile } from 'fs/promises';
-const app = express();
+//import { readFile } from 'fs/promises';
+import express from "express";
 
-// Porta do servidor
+const posts = [
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150" },
+];
+
+const app = express();
+app.use(express.json());
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Bem vindo o projeto aula 1 imersão Minha Experiência na Aula de Imersão Dev Back - End: Desvendando APIs e Servidores.");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
 
-app.get("/livro", async(req, res) => {
-    try {
-        const data = await readFile("livro.json", "utf8");
-        res.status(200).json(JSON.parse(data));
-    } catch (err) {
-        console.error("Erro ao ler o arquivo:", err);
-        res.status(500).send("Erro ao carregar o livro.");
-    }
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
 });
+//app.get("/livro", async(req, res) => {
+//    try {
+//        const data = await readFile("livro.json", "utf8");
+//        res.status(200).json(JSON.parse(data));
+//    } catch (err) {
+//        console.error("Erro ao ler o arquivo:", err);
+//        res.status(500).send("Erro ao carregar o livro.");
+//    }
+//});
+
+//app.get("/api", (req, res) => {
+//           res.status(200).send("Bem vindo o projeto aula 1 imersão Minha Experiência na Aula de Imersão Dev Back - End: Desvendando APIs e Servidores.");
+//});
