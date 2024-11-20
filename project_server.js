5a45b933fd3660107e92c9d037507bbb1db75bad
alanbazilik <alangoncalvesbazilio@gmail.com>
2024-11-20 17:06:44 -0300

    Aula 03 - finalizada ✅

--- project_server.js
+++ project_server.js
@@ -1,33 +1,15 @@
 //import { readFile } from 'fs/promises';
 import express from "express";
-
-const posts = [
-    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
-    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
-    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150" },
-];
-
+import routes from "./src/routes/postsRoutes.js";
 const app = express();
-app.use(express.json());
+
+routes(app);
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorID(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    })
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
-});
 //app.get("/livro", async(req, res) => {
 //    try {
 //        const data = await readFile("livro.json", "utf8");
